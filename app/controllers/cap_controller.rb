1402aa3d0bfab07010cbe8e3527098215e778ce6
Andrey Pachay <a.pachay@ailove.ru>
2012-09-10 23:16:50 +0400

    jenkins widget

--- app/controllers/cap_controller.rb
+++ app/controllers/cap_controller.rb
@@ -1,7 +1,6 @@
 class CapController < ApplicationController
   unloadable
 
-
   def deploy
     @project = Project.find(params[:project_id])
     @result = `/srv/admin/bin/cap.sh #{@project.identifier.to_s} ailove:factory_deploy 2>&1`
@@ -19,6 +18,5 @@ class CapController < ApplicationController
 
   def index
     @project = Project.find(params[:project_id])
-    @jenkins_widget = `/usr/bin/curl -n http://test.ailove.ru:8080/view/Experimental/job/#{@project.identifier.to_s}/jswidgets/health?skipDescription=true`
   end
 end
